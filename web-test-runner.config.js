0bffe9c4c9a0016fabb0b51b829adcefd6977879
Elbert Oh <ellokri89@gmail.com>
2022-12-28 10:26:17 -0500

    add multi browser testing

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,4 +1,5 @@
 import { esbuildPlugin } from "@web/dev-server-esbuild";
+import { playwrightLauncher } from "@web/test-runner-playwright";
 
 export default {
   files: ["**/*.spec.ts", "**/*.test.ts"],
@@ -7,6 +8,11 @@ export default {
       ts: true,
       tsconfig: "tests/tsconfig.json"
     })
+  ],
+  browsers: [
+    playwrightLauncher({ product: "chromium" }),
+    // playwrightLauncher({ product: "webkit" }), // not supported in Fedora yet
+    playwrightLauncher({ product: "firefox" })
   ],
   nodeResolve: true,
   debug: false,
