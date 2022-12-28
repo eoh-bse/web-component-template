43a65b7676943eb936106d7ba1040b05b38e40dd
Elbert Oh <ellokri89@gmail.com>
2022-12-28 10:26:17 -0500

    update watcher to wait until the typescript files are all compiled before launching web dev server

--- build-scripts/watchers/watcher.js
+++ build-scripts/watchers/watcher.js
@@ -1,16 +1,20 @@
 import { spawn } from "child_process";
+import { getAllFilesMatchingRegex } from "../utils/file-finder.js";
 import { cleanDirectory } from "../utils/directory-cleaner.js";
 import BuildConfigSingleton from "../builders/build-config.js";
+
+function ensureCompilationCompletion(tsOutDir) {
+  let tsCompilationDone = false;
+  while (!tsCompilationDone) {
+    const jsFiles = getAllFilesMatchingRegex(tsOutDir, /.*\.js$/);
+    tsCompilationDone = jsFiles.length > 0;
+  }
+}
 
 const buildConfig = await BuildConfigSingleton.instance.getOrCreate();
 
 await cleanDirectory(buildConfig.target);
 
-spawn("node ./build-scripts/watchers/html-file-watcher.js", { shell: true });
-spawn("node ./build-scripts/watchers/css-file-watcher.js", {
-  shell: true,
-  stdio: "inherit"
-});
 spawn(
   "node_modules/typescript/bin/tsc",
   ["--watch", `--outDir ${buildConfig.target}`],
@@ -20,7 +24,20 @@ spawn(
   }
 );
 
+spawn("node ./build-scripts/watchers/html-file-watcher.js", {
+  shell: true,
+  stdio: "inherit"
+});
+spawn("node ./build-scripts/watchers/css-file-watcher.js", {
+  shell: true,
+  stdio: "inherit"
+});
+
+console.log("Ensuring typescript compilation is finished...");
+ensureCompilationCompletion(buildConfig.target);
+
 spawn(`npx web-dev-server --root-dir ${buildConfig.target} --watch --open`, {
   shell: true,
   stdio: "inherit"
 });
+
