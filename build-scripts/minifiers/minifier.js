b210bebf12b617b53a1379506a3c64d64fc0215b
Elbert Oh <ellokri89@gmail.com>
2023-02-16 11:45:42 -0500

    fix minify script to only read and write html, css, js files

--- build-scripts/minifiers/minifier.js
+++ build-scripts/minifiers/minifier.js
@@ -1,6 +1,6 @@
 import fs from "fs/promises";
 
-import { getAllFiles } from "../utils/file-finder.js";
+import { getAllFilesMatchingRegex } from "../utils/file-finder.js";
 import { minifyHtml } from "./html-minifier.js";
 import { minifyCss } from "./css-minifier.js";
 import { minifyJs } from "./js-minifier.js";
@@ -8,8 +8,8 @@ import { minifyJs } from "./js-minifier.js";
 const emptyFileContent = "export {};";
 
 async function minify(buildFolder) {
-  const allFiles = getAllFiles(buildFolder);
-  const getFileContents = allFiles.map(async file => {
+  const filesToMinify = getAllFilesMatchingRegex(buildFolder, /.*\.(html|css|js)$/);
+  const getFileContents = filesToMinify.map(async file => {
     const content = await fs.readFile(file, { encoding: "utf-8" });
     return {
       path: file,
